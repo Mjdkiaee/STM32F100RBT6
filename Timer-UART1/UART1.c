8b0b08ac8d639a28e8bf5c6bde94dd46ae8ce418
Mjdkiaee <62328887+Mjdkiaee@users.noreply.github.com>
2020-05-10 17:16:31 +0430

    Add files via upload

---
+++ Timer-UART1/UART1.c
@@ -0,0 +1,16 @@
+#include "UART1.h"
+
+extern UART_HandleTypeDef huart1;
+
+char get_char(void){
+	char ch[1];
+	HAL_UART_Receive(&huart1,(unsigned char*)ch,1,200000);
+	return ch[0];
+	}
+void PutString(char* str){
+	while(*str !='\0' )
+	 {
+		 HAL_UART_Transmit(&huart1,(unsigned char*) str,1,10);
+		 str++;
+	 }
+}
